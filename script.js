ebb8a06e1e4654741c1b995e0778f12190d004ac
Stamp7788 <93626883+Stamp7788@users.noreply.github.com>
2024-05-13 23:50:04 +0000

    Johnny CSS stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+const generalMenu = {
+  currentMenu: null,
+
+  closeMenu: function(menuID) {
+    let menuDom = document.getElementById(menuID);
+    menuDom.style.zIndex = -1;
+    },
+
+  openMenu: function(menuID) {
+    let menuDom = document.getElementById(menuID);
+    menuDom.style.zIndex = 1;
+  },
+
+  
+
+};
