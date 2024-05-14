24b99eb32a77908f8d37d55570abc47600b460ac
Stamp7788 <93626883+Stamp7788@users.noreply.github.com>
2024-05-14 00:00:55 +0000

    Formatted HTML and CSS

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const generalMenu = {
   closeMenu: function(menuID) {
     let menuDom = document.getElementById(menuID);
     menuDom.style.zIndex = -1;
-    },
+  },
 
   openMenu: function(menuID) {
     let menuDom = document.getElementById(menuID);
     menuDom.style.zIndex = 1;
   },
 
-  
+
 
 };
